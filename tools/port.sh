3a77c4237173fdb6a053d73d48f8bc0b5420caec
Emil Skogsund <emil@skogsund.se>
2025-12-01 18:18:14 +0100

    workstation-ending

--- tools/port.sh
+++ tools/port.sh
@@ -3,18 +3,19 @@ set -e
 
 # @describe Takes an open/available port between 8000 and 8100
 # @option --port![`_choice_port`] <NUM> An available port between 8000 and 8100
+# @env LLM_OUTPUT=/dev/stdout The output path
 
 main() {
-    echo "$argc_port"
+  echo "port is ok: $argc_port" >>$LLM_OUTPUT
 }
 
 _choice_port() {
-    local used_ports=$(ss -tuln 2>/dev/null | awk '{print $5}' | grep -oE '[0-9]+$' | sort -u)
-    for port in $(seq 8000 8100); do
-        if ! echo "$used_ports" | grep -q "^${port}$"; then
-            echo "$port"
-        fi
-    done
+  local used_ports=$(ss -tuln 2>/dev/null | awk '{print $5}' | grep -oE '[0-9]+$' | sort -u)
+  for port in $(seq 8000 8100); do
+    if ! echo "$used_ports" | grep -q "^${port}$"; then
+      echo "$port"
+    fi
+  done
 }
 
 eval "$(argc --argc-eval "$0" "$@")"
